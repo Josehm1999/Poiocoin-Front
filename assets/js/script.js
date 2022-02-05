4b79599597497d9d04dc7268dc56583a548403ec
JoseHM <josehm1999@gmail.com>
2022-02-04 19:39:02 -0600

    Feat(index): Add Hero section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
 const headerMenu = document.getElementById("headerMenu");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 headerMenu.onclick = () => {
-  if (headerMenu.classList.contains("open")) {
-    headerMenu.classList.remove("open");
+  console.log("Funciona :D");
+  if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
   } else {
-    headerMenu.classList.add("open");
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
 };
 
